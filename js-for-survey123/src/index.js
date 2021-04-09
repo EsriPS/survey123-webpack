17f5867fe2e5d8cadc043d0695a9e3a16381b6cd
Travis Butcher <tbutcher@esri.com>
2021-04-09 15:30:09 -0700

    Update with getCentroid

--- js-for-survey123/src/index.js
+++ js-for-survey123/src/index.js
@@ -1,11 +1,23 @@
-import { console, globals, fetchSync } from './survey123/lib.js';
-import txt from './test.txt';
-import main from './main.js';
-import proj4 from 'proj4';
+import { console, globals, fetchSync } from "./survey123/lib.js";
+import txt from "./test.txt";
+import main from "./main.js";
+import proj4 from "proj4";
+import QRCode from "qrcode";
+
+//Add Helpers for Turf
+var helpers = require("@turf/helpers");
+import centroid from "@turf/centroid";
 
 export function importText() {
   console.log(`\`${globals.libraryName}.importText\` method invoked`);
   return txt;
+}
+
+export function HelloWorld(language) {
+  console.log("Reached Hello World");
+  if (language == "en") return JSON.stringify({ Hello: "World" });
+  else if (language == "es") return JSON.stringify({ Hola: "Mundo" });
+  else return JSON.stringify({ error: "No Language Selected" });
 }
 
 export function importJson() {
@@ -13,28 +25,75 @@ export function importJson() {
   return main().object.string;
 }
 
-export function fetchTest() {
-  console.log(`\`${globals.libraryName}.importText\` method invoked`);
-  return fetchSync('http://aero.esri.com/arcgis/rest/services?f=pjson').json()
-    .currentVersion;
-}
-
-export function reprojectPoint(x, y) {
+function createPolygons(polygons) {
   try {
-    if (isNaN(x) || isNaN(y)) {
-      return 'Please provide numbers for x or y';
+    var p = JSON.stringify(polygons).replace(/,0]/g, "]");
+    //Loop through the Polygons and Build Turf Polygons
+    var polygon = JSON.parse(p);
+    //return polygons[0].hasOwnProperty('rings');
+    //return polygon
+    var output = [];
+    var rings;
+    for (var i = 0; i < polygon.length; i++) {
+      for (var r = 0; r < polygon[i].rings.length; r++) {
+        rings = polygon[i].rings[r];
+        rings.push(rings[0]);
+        output.push(rings);
+      }
     }
 
-    if (!isFinite(x) || !isFinite(y)) {
-      return 'Please provide finite numbers for x or y';
+    var checkPolygon = helpers.polygon(output);
+    return checkPolygon;
+  } catch (e) {
+    return { message: e.message };
+  }
+}
+
+function groupPolygons(polygons) {
+  if (polygons.length != 0) {
+    try {
+      return JSON.stringify(polygons);
+    } catch (e) {
+      return JSON.stringify({ error: e.message });
+    }
+  } else {
+    return JSON.stringify({ error: "Must have at least one polygon in Area" });
+  }
+}
+
+export function getCentroid(polygons) {
+  try {
+    //Split the JSON at the |
+    var splitPolygons = JSON.parse("[" + polygons.replace("|", ",") + "]");
+
+    try {
+      var checkPolygon = createPolygons(splitPolygons);
+      var checkCentroid = centroid(checkPolygon);
+
+      return checkCentroid.geometry.coordinates.reverse().join(" ");
+    } catch (e) {
+      return JSON.stringify({ error: e.message });
+    }
+  } catch (e) {
+    return JSON.stringify({
+      error: e.message,
+    });
+  }
+}
+
+export function reprojectPoint(location) {
+  try {
+    if (location == null) {
+      return JSON.stringify({ error: "Please select a location on the map" });
     }
 
     //https://spatialreference.org/ref/epsg/indian-1960-utm-zone-48n/proj4/
     var projectTo =
-      '+proj=utm +zone=48 +a=6377276.345 +b=6356075.41314024 +units=m +no_defs';
-    return proj4(projectTo, [x, y]);
+      "+proj=utm +zone=48 +a=6377276.345 +b=6356075.41314024 +units=m +no_defs";
+    var coords = proj4(projectTo, [location.x, location.y]);
+    return JSON.stringify({ x: coords[0], y: coords[1] });
   } catch (e) {
-    return '';
+    return JSON.stringify({ error: e.message });
   }
 }
 
@@ -42,6 +101,6 @@ export function getConsole() {
   return console.getOutput({ sizeLimit: 4096 });
 }
 
-export { dev } from './survey123/lib.js';
+export { dev } from "./survey123/lib.js";
 
-export { debugTerminal } from './survey123/lib.js';
+export { debugTerminal } from "./survey123/lib.js";
